df35f38713ff28043ae32fcfc707c7cb3b07ff4f
Peregrino Y. Mendoza <pymendoza@gmail.com>
2014-12-15 12:10:42 +0800

    fixed xpath selector on Transaction nodes

--- source/OFXSharp/Transaction.cs
+++ source/OFXSharp/Transaction.cs
@@ -46,14 +46,14 @@ namespace OFXSharp
 
       public Transaction(XmlNode node, string currency)
       {
-         TransType = GetTransactionType(node.GetValue("//TRNTYPE"));
-         Date = node.GetValue("//DTPOSTED").ToDate();
-         TransactionInitializationDate = node.GetValue("//DTUSER").ToDate();
-         FundAvaliabilityDate = node.GetValue("//DTAVAIL").ToDate();
+         TransType = GetTransactionType(node.GetValue(".//TRNTYPE"));
+         Date = node.GetValue(".//DTPOSTED").ToDate();
+         TransactionInitializationDate = node.GetValue(".//DTUSER").ToDate();
+         FundAvaliabilityDate = node.GetValue(".//DTAVAIL").ToDate();
 
          try
          {
-            Amount = Convert.ToDecimal(node.GetValue("//TRNAMT"), CultureInfo.InvariantCulture);
+            Amount = Convert.ToDecimal(node.GetValue(".//TRNAMT"), CultureInfo.InvariantCulture);
          }
          catch (Exception ex)
          {
@@ -62,45 +62,45 @@ namespace OFXSharp
 
          try
          {
-            TransactionID = node.GetValue("//FITID");
+            TransactionID = node.GetValue(".//FITID");
          }
          catch (Exception ex)
          {
             throw new OFXParseException("Transaction ID unknown", ex);
          }
 
-         IncorrectTransactionID = node.GetValue("//CORRECTFITID");
+         IncorrectTransactionID = node.GetValue(".//CORRECTFITID");
 
 
          //If Transaction Correction Action exists, populate
-         var tempCorrectionAction = node.GetValue("//CORRECTACTION");
+         var tempCorrectionAction = node.GetValue(".//CORRECTACTION");
 
          TransactionCorrectionAction = !String.IsNullOrEmpty(tempCorrectionAction)
                                           ? GetTransactionCorrectionType(tempCorrectionAction)
                                           : TransactionCorrectionType.NA;
 
-         ServerTransactionID = node.GetValue("//SRVRTID");
-         CheckNum = node.GetValue("//CHECKNUM");
-         ReferenceNumber = node.GetValue("//REFNUM");
-         Sic = node.GetValue("//SIC");
-         PayeeID = node.GetValue("//PAYEEID");
-         Name = node.GetValue("//NAME");
-         Memo = node.GetValue("//MEMO");
+         ServerTransactionID = node.GetValue(".//SRVRTID");
+         CheckNum = node.GetValue(".//CHECKNUM");
+         ReferenceNumber = node.GetValue(".//REFNUM");
+         Sic = node.GetValue(".//SIC");
+         PayeeID = node.GetValue(".//PAYEEID");
+         Name = node.GetValue(".//NAME");
+         Memo = node.GetValue(".//MEMO");
 
          //If differenct currency to CURDEF, populate currency 
-         if (NodeExists(node, "//CURRENCY"))
-            Currency = node.GetValue("//CURRENCY");
-         else if (NodeExists(node, "//ORIGCURRENCY"))
-            Currency = node.GetValue("//ORIGCURRENCY");
+         if (NodeExists(node, ".//CURRENCY"))
+            Currency = node.GetValue(".//CURRENCY");
+         else if (NodeExists(node, ".//ORIGCURRENCY"))
+            Currency = node.GetValue(".//ORIGCURRENCY");
             //If currency not different, set to CURDEF
          else
             Currency = currency;
 
          //If senders bank/credit card details avaliable, add
-         if (NodeExists(node, "//BANKACCTTO"))
-            TransactionSenderAccount = new Account(node.SelectSingleNode("//BANKACCTTO"), AccountType.BANK);
-         else if (NodeExists(node, "//CCACCTTO"))
-            TransactionSenderAccount = new Account(node.SelectSingleNode("//CCACCTTO"), AccountType.CC);
+         if (NodeExists(node, ".//BANKACCTTO"))
+            TransactionSenderAccount = new Account(node.SelectSingleNode(".//BANKACCTTO"), AccountType.BANK);
+         else if (NodeExists(node, ".//CCACCTTO"))
+            TransactionSenderAccount = new Account(node.SelectSingleNode(".//CCACCTTO"), AccountType.CC);
       }
 
       /// <summary>
